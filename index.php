e989b36869375691aa63e9674b13fdf2d0d6f61e
ShadowTheBest <101932050+ShadowTheBest@users.noreply.github.com>
2022-07-14 15:28:31 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+    <link rel="icon" type="image/png" href="./css/img/AC_Serveur_Icone.png">
+    <title>L'aventure-crafteuse</title>
+</head>
+<body>
+    <h1>L'aventure Crafteuse</h1>
+    <nav>
+        <div id="mdn">
+            <p>la partie connexion/inscription arrivera plus tard suite a de nombreux bugs</p>
+                <a href="">
+                    <p id="connexion">
+                        Connexion
+                    </p>
+                </a>
+            <a href="">
+                <p id="inscription">
+                    Inscription
+                </p>
+            </a>
+        </div>
+    </nav>
+    <p>
+
+        L'aventure crafteuse est un serveur 100% RP encore en construction. Ce serveur est destiné à toutes personnes souhaitant faire des RolePlay Avec leurs amis ou même la communauté du <a href="https://discord.gg/ubuMyWq9" target="__BLANK__">Discord</a>. Notre Objectif est donc de créer un serveur pour que n'importe qui puisse faire ses RolePlay sans être dérangé.
+    <p>
+        Le serveur possède un systeme de chambres d'hotel a réserver sur le site et possibilité de louer/acheter des maisons dans la ville. Il faut donc contacter l'agence immobiliaire en envoyant un mail avec la commande /mail send nomdujoueur puis attendre une réponse ce qui peut prendre quelques heures.
+
+    </p>
+    <p>
+        Nous vous attendons sur le serveur avec impacience et bon RP a vous. 
+    </p>
+    <p>
+        Un petit point sur les règles:
+            
+            Toutes tentative de glich pour récuperer des permissions sera sanctioné par un ban de 3 mois ou un ban à vie si c'est son 2eme ban.<br/>
+            Tout skin/pseudo/message innaproprié sera banni/mute pendant une durée de 1 semaine ou 3 mois si 2eme ban puis ban permanant au bout du 3eme.<br/>
+            Tout grief/constructions inapropriés seront sanctionées par 3 mois de ban et ban permanant si cela se reproduit.<br/>
+    </p>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+
+    <div id="ip"><p>IP:</p><p id="ip">citywithoutname.aternos.me</p><br></div>
+</body>
+</html>
